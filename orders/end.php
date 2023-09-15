35c0b0c02dfb354c76cf3f1cc60c4445bcaec163
random_guy <110307181+Phensteve@users.noreply.github.com>
2023-09-15 11:27:01 +0100

    Create end.php for the order pages

---
+++ orders/end.php
@@ -0,0 +1,13 @@
+<style>
+    .bottom {
+        position: fixed;
+        right: 23px;
+        bottom: 5px;
+        padding-top: 15px;
+        margin-bottom: 0;
+        z-index: 997;
+    }
+</style>
+<span class="bottom text-secondary text-gray">
+    © Designed by Oluyo😉
+</span>
